59e9f8dcb25c287949620ae166f628c1949299d1
Ossama <odahbi@myges.fr>
2021-06-10 14:17:09 +0200

    Application Java

---
+++ mission_2/src/scene/Main.java
@@ -0,0 +1,23 @@
+package scene;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        Parent root = FXMLLoader.load(getClass().getResource("projet/projet_user.fxml"));
+        primaryStage.setTitle("Connexion");
+        primaryStage.setScene(new Scene(root, 669, 483));
+        primaryStage.show();
+    }
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
